d603d9f4b470ae677ffad3135aa3c139134e20a8
Charles Kelsey <130581038+charleskelsey@users.noreply.github.com>
2025-10-09 14:33:34 -0400

    adding localization and cca

---
+++ license-plate-recognition/cca.py
@@ -0,0 +1,28 @@
+from skimage import measure
+from skimage.measure import regionprops
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import localization
+
+# CCA basically helps us group and label connected regions on the foreground. 
+# A pixel is deemed to be connected to another if they both have the same value 
+# and are adjacent to each other.
+
+# this gets all the connected regions and groups them together
+label_image = measure.label(localization.binary_car_image)
+fig, (ax1) = plt.subplots(1)
+ax1.imshow(localization.gray_car_image, cmap="gray");
+
+# regionprops creates a list of properties of all the labelled regions
+for region in regionprops(label_image):
+    if region.area < 50:
+        #if the region is so small then it's likely not a license plate
+        continue
+
+    # the bounding box coordinates
+    minRow, minCol, maxRow, maxCol = region.bbox
+    rectBorder = patches.Rectangle((minCol, minRow), maxCol-minCol, maxRow-minRow, edgecolor="red", linewidth=2, fill=False)
+    ax1.add_patch(rectBorder)
+    # let's draw a red rectangle over those regions
+
+plt.show()
